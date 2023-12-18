462fe4383d89e805c497aa251d57e155f63da7be
Diego <sarmiento.df@gmail.com>
2023-12-17 20:55:29 -0500

    implementación de autenticación

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "@/components/navbar";
 import { Link } from "@nextui-org/link";
 import { Head } from "./head";
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 export default function DefaultLayout({
 	children,
@@ -11,9 +12,11 @@ export default function DefaultLayout({
 		<div className="relative flex flex-col h-screen">
 			<Head />
 			<Navbar />
-			<main className="container mx-auto max-w-7xl px-6 flex-grow">
-				{children}
-			</main>
+			<UserProvider>
+				<main className="container mx-auto max-w-7xl px-6 flex-grow">
+					{children}
+				</main>
+			</UserProvider>
 			<footer className="w-full flex items-center justify-center py-3">
 				<Link
 					isExternal
